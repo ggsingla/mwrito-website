218389ef6b947afb9ccb9a6c8fc05727437abf00
Adityagupta1625 <guptaaditya2512@gmail.com>
2021-10-31 12:15:53 +0530

    backend of blogs

--- src/components/Blogs/BlogCard.js
+++ src/components/Blogs/BlogCard.js
@@ -1,8 +1,19 @@
 import { FaAngleRight } from "react-icons/fa";
 import { StyledBlogCard } from "../styles/Blogs/LatestBlogs.styled";
+import db from "./firebase";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
+async function getData() {
+  const blog = collection(db, 'Blogs');
+  const blogSnapShot = await getDocs(blog);
+  const blogList = blogSnapShot.docs.map(
+    doc => doc.data());
+  console.log(blogList);
+}
 export const BlogCard = () => {
+  getData();
   return (
+    
     <StyledBlogCard>
       <img src="https://source.unsplash.com/random" alt="Blog Image" />
       <div className="content">
@@ -16,5 +27,6 @@ export const BlogCard = () => {
         <a href="/">More Details <FaAngleRight/></a>
       </div>
     </StyledBlogCard>
+    
   );
 };
---
+++ src/components/Blogs/firebase.js
@@ -0,0 +1,17 @@
+import {initializeApp} from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: "writo-app",
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId:process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+const app=initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
